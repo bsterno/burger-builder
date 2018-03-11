4d1701d2836056d4586b0aa58f362574b042e774
Brian Stern <brian@brickworksoftware.com>
2018-03-11 17:51:11 -0400

    outputting ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+  const ingredientsArray = Object.keys(props.ingredients);
+  console.log(ingredientsArray);
+
+  const burgerIngredients = ingredientsArray.map(ingredientKey => {
+    return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+      console.log(i);
+      console.log(ingredientKey);
+      return <BurgerIngredient key={ingredientKey + i} type={ingredientKey} />;
+    });
+  });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {burgerIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
